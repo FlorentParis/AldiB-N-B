9bed5ff1ab12e31ab3e8b7009f2e61b36fb50753
Maxence Nho <78363268+PiKwaii93@users.noreply.github.com>
2022-03-05 12:54:26 +0100

    Roles

--- wordpress/wp-content/themes/aldibnb/functions.php
+++ wordpress/wp-content/themes/aldibnb/functions.php
@@ -62,5 +62,121 @@ function createUser()
     ));
 };
 
+function wphetic_register_style_taxonomy(){
+    
+    $labels = [
+        'name' => 'Styles',
+        'singular_name' => 'Style',
+        'search_items' => 'Rechercher style',
+        'all_items' => 'Tous les styles'
+    ];
+
+    $args = [
+        'labels' => $labels,
+        'public' => true,
+        'hierarchical' => true,
+        'show_in_rest' => true,
+        'show_admin_column' => true
+    ];
+
+    register_taxonomy('style', ['post'], $args);
+
+}
+
+function wphetic_register_event_cpt(){
+    
+    $labels = [
+        'name' => 'Evènements',
+        'singular_name' => 'Evènements',
+        'search_items' => 'Rechercher évènements',
+        'all_items' => 'Tous les évènements'
+    ];
+
+    $args = [
+        'labels' => $labels,
+        'public' => true,
+        'show_in_rest' => true,
+        'menu_icon' => 'dashicon-tickets',
+        'supports' => ['title', 'editor', 'excerpt', 'thumbnail', 'custom-fields'],
+        'has_archive' => true,
+        'taxonomies' => ['style'],
+        'capabilities' => array(
+            'edit_post' => 'event_rights',
+            'read_post' => 'event_rights',
+            'delte_post' => 'event_rights',
+            //On définit quel droit permet de faire tel action
+        ),
+    ];
+
+    register_post_type('event', $args);
+
+}
+
+
 add_action('after_setup_theme','wphetic_theme_support');
+
+/*Ajout de bootstrap*/
 add_action('wp_enqueue_scripts','wphetic_bootstrap');
+
+/*Ajout de la taxonomie*/
+add_action('init','wphetic_register_style_taxonomy');
+
+/*Ajout d'un terme à la taxonomie 'style'*/
+add_action('after_switch_theme', function() {
+    wp_insert_term('Dubstep', 'style'); //Insertion du terme 'Dubstep' dans la taxonomie 'style'
+    flush_rewrite_rules(); //Empêche les bugs de réécriture d'URL
+});
+
+/*Ajout de post custom*/
+add_action('init','wphetic_register_event_cpt');
+
+/*Ajout des droits 'manage_events' à l'utilisateur 'administrator'*/
+add_action('after_switch_theme', function(){
+    $admin = get_role('administrator'); //Récupération du rôle 'administrator
+    $admin->add_cap('manage_events'); //Ajout des droits au rôle
+});
+
+/*Ajout du rôle Event Manager*/
+add_action('after_switch_theme', function() {
+    add_role('event_manager', 'Event Manager', [  //Création du rôle
+        'read' => true, //Ajout du droit pour lire
+        'event_rights' => true //Ajout du droit pour gérer les events
+    ]);
+});
+
+/*Ajout du rôle Utilisateur*/
+add_action('after_switch_theme', function() {
+    add_role('utilisateur', 'Utilisateur', [  //Création du rôle
+        'event_rights' => true, //Ajout du droit pour gérer les events
+        'delete_posts' => true,
+        'edit_posts' => true,
+        'publish_posts' => true
+        //C'est ici qu'on définiera les droits de nos utilisateurs
+    ]);
+});
+
+/*Ajout du rôle Manager*/
+add_action('after_switch_theme', function() {
+    add_role('manager', 'Manager', [  //Création du rôle
+        'read' => true, //Ajout du droit pour lire
+        'event_rights' => true, //Ajout du droit pour gérer les events
+        'delete_posts' => true,
+        'edit_posts' => true,
+        'publish_posts' => true,
+        'delete_others_posts' => true,
+        'delete_published_posts' => true,
+        'delete_private_posts' => true,
+        'edit_others_posys' => true,
+        'edit_private_posts' => true,
+        'edit_published_posts' => true,
+        'read_privates_posts' => true
+        //C'est ici qu'on définiera les droits de nos utilisateurs
+    ]);
+});
+
+/*Nettoyer les droits donnés aux utilisateurs pour ne pas entacher les autres thèmes*/
+add_action('switch_theme', function() {
+    $admin = get_role('administrator'); //Récupération du rôle 'administrator
+    $admin->remove_cap('utilisateur'); //Suppression des droits de l'utilisateur
+    remove_role('utilisateur'); //Suppression du droit 
+});
--- wordpress/wp-content/themes/aldibnb/index.php
+++ wordpress/wp-content/themes/aldibnb/index.php
@@ -1,5 +1,18 @@
 <?php get_header(); ?>
 
+<div class="mt-3 mb-3">
+    <h3>Catégories</h3>
+    <ul class="list-group list-group-horizontal text-center">
+        <?php
+        $terms = get_terms(['taxonomy' => 'style']);
+        foreach ($terms as $term) {
+            $active = get_query_var('style') === $term->slug ? active : '';
+            echo '<a class="list-group-iteù list-item-action'. $active .'
+            "href="' . get_term_link($term) . '">' . $term->name . '</a>';
+        }; ?>
+</div>
+
+
 <?php if (have_posts()) : ?>
 <div class="card-group">
     <?php while (have_posts()) : ?>
@@ -19,7 +32,7 @@
                 <h5 class="card-title"><?php the_title(); ?></h5>
 
                 <p><small> Style: <?= the_terms(get_the_ID(), 'style'); ?></small></p>
-
+                <p class="card-text"><?php the_excerpt(); ?></p>
                 <p class="card-text"><?php the_content(); ?></p>
                 <a href="<?php the_permalink(); ?>" class="btn btn-primary">Lire plus</a>
             </div>
